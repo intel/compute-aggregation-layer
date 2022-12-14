acf5ec0fa76b8f25b10dbe20cb4cccbbf6e98309
Luzynski, Sebastian Jozef <sebastian.jozef.luzynski@intel.com>
2022-12-14 15:59:12 +0000

    Update README.md
    Add information about available build flags, settings and debug
    variables
    
    Signed-off-by: Luzynski, Sebastian Jozef <sebastian.jozef.luzynski@intel.com>

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ SPDX-License-Identifier: MIT
 # Compute Aggregation Layer for oneAPI Level Zero and OpenCL(TM) Applications
 
 ## Introduction
-Compute Aggregation Layer for oneAPI Level Zero and OpenCL(TM) Applications is an IPC-based mechanism for rerouting L0/OCL API calls from multiple processes into one (into Aggregation Layer service) in order to reduce overhead of GPU context switches.
+Compute Aggregation Layer for oneAPI Level Zero and OpenCL(TM) Applications is an IPC-based mechanism for rerouting L0/OCL API calls from multiple processes into an aggregation service, in order to reduce overhead of GPU context switches. It's tested with Intel(R) Graphics Compute Runtime for oneAPI Level Zero and OpenCL(TM) Driver, whose versions are provided in [third_party/linux_dependencies.yml](third_party/linux_dependencies.yml).
 
 ## License
 The Compute Aggregation Layer for oneAPI Level Zero and OpenCL(TM) Applications is distributed under the MIT License.
@@ -15,14 +15,6 @@ The Compute Aggregation Layer for oneAPI Level Zero and OpenCL(TM) Applications 
 You may obtain a copy of the License at: https://opensource.org/licenses/MIT
 
 We at Intel are commited to create an open, welcoming, diverse, inclusive, and healthy community. Please refer to our [Contributor Covenant Code of Conduct](CODE_OF_CONDUCT.md) for full text of our pledge and standards.
-
-## Supported workloads
-
-```<this list is currently empty>```
-
-Development of Compute Aggregation Layer is workload-driven. Along with new improvements to Compute Aggregation Layer, this section will be updated to contain a support matrix of workload/platform combinations that were enabled and tested.
-
-Applications not listed in the support matrix may or may not work with Compute Aggregation Layer.
 
 ## Building and Installing
 To build Compute Aggregation Layer, a Linux machine with C++17 compliant compiler is required. It also requires [CMake](https://cmake.org/) build system. Following commands will produce Release build. If you would like to build in Debug mode, add `-DCMAKE_BUILD_TYPE=Debug` flag to cmake step.
@@ -47,6 +39,12 @@ You may also need to run ldconfig to update shared libraries cache for newly ins
 ```bash
 sudo ldconfig
 ```
+
+Additional cmake build flags, that can be set during cmake invoke step (`-D<FLAG_NAME>=<VALUE>`):
+- `ENABLE_BLOATED_VERBOSITY` - default: `false`. Control whether `bloat` log verbosity should be available
+- `ENABLE_PERFORMANCE_DEBUG` - default: `true`. Control whether performance-related debug logs should be available
+- `REQUIRE_CACHE_VERIFICATION_DEPENDENCIES` - default: `false`. Causes failure of build when the verification of generated cache cannot be performed
+- `VERIFY_GENERATED_CACHE_IF_POSSIBLE` - default: `true`. Verifies if cached generated files are up to date and in synch with source files
 
 For development, we use Ubuntu 20.04 native and WSL, with gcc-9 and clang-14.
 
@@ -80,6 +78,9 @@ source /opt/compute-aggregation-layer/cal_setvars.sh
 ```
 
 After that, run target application as you would normally do.
+
+## Available enviroment flags
+Refer to [include/cal.h](include/cal.h) for available flags and their descriptions.
 
 ## See also
 - [Intel(R) Graphics Compute Runtime for oneAPI Level Zero and OpenCL(TM) Driver](https://github.com/intel/compute-runtime)
--- include/cal.h
+++ include/cal.h
@@ -19,28 +19,44 @@ constexpr std::string_view calTempFilesDefaultPath = "/tmp/compute_aggregation_l
 constexpr std::string_view calShmemPathPrefix = "/compute_aggregation_layer_shmem_";
 constexpr std::string_view calListenerSocketPathEnvName = "CAL_LISTENER_SOCKET_PATH";
 
+// Controls whether Level Zero API calls should go through CAL
 constexpr std::string_view calEnableL0InCalrunEnvName = "CAL_ENABLE_L0_IN_CALRUN";
+// Controls whether OpenCL API calls should go through CAL
 constexpr std::string_view calEnableOclInCalrunEnvName = "CAL_ENABLE_OCL_IN_CALRUN";
 
+// Controls whether some API calls should cache their results for faster access
 constexpr std::string_view calIcdEnableCacheEnvName = "CAL_ICD_ENABLE_CACHE";
 constexpr std::string_view calEnableAilEnvName = "CAL_USE_AIL";
+// Controls whether CAL should use shared allocations
 constexpr std::string_view calSharedAllocations = "CAL_SHARED_ALLOCATIONS";
+// Controls whether threads should yield control while waiting for connection
 constexpr std::string_view calYieldThreadsEnvName = "CAL_YIELD_THREADS";
+// Controls whether CAL should reuse buffers
 constexpr std::string_view calIcdBufferRecycleEnvName = "CAL_ICD_BUFFER_RECYCLE";
+// Set default RPC channel size (in MB)
 constexpr std::string_view calDefaultRpcChannelSizeEnvName = "CAL_DEFAULT_RPC_CHANNEL_SIZE_MB";
 
 constexpr std::string_view calPlatformName = "Compute Aggregation Layer for oneAPI Level Zero and OpenCL(TM) Applications";
 constexpr std::string_view calServiceProcessName = NAME_TO_STR(CAL_NAME);
 
+// Controls whether CAL should use semaphores as a service synchronization method
 constexpr std::string_view calUseSemaphoresInChannelServerEnvName = "CAL_USE_SEMAPHORES_IN_CHANNEL_SERVER";
+// Controls whether CAL should use semaphores as a client synchronization method
 constexpr std::string_view calUseSemaphoresInChannelClientEnvName = "CAL_USE_SEMAPHORES_IN_CHANNEL_CLIENT";
 constexpr std::string_view calUseSemaphoresThresholdInChannelClientEnvName = "CAL_USE_SEMAPHORES_THRESHOLD_IN_CHANNEL_CLIENT";
 
 // Debug
+// Sets required logging verbosity. Available levels: [performance, silent, critical, error, info, debug, bloat]. Warning: bloat verbosity requires CAL to be built with ENABLE_BLOATED_VERBOSITY=1 cmake option
 constexpr std::string_view calVerbosityEnvName = "CAL_MAX_VERBOSITY";
+// Controls whether CAL should work in benchmarking mode, which prints performance logs for aggregation layer
 constexpr std::string_view calBenchmarkEnvName = "CAL_BENCHMARK";
+// Set name of logger that prints results to stdout
 constexpr std::string_view calUseLoggerNameEnvName = "CAL_USE_LOGGER_NAME";
+// Appends PID to logger, for easier debug of multi process applications
 constexpr std::string_view calAppendPIDEnvName = "CAL_APPEND_PID";
+// Controls OpenCL profiling
 constexpr std::string_view calOclDisableProfilingEnvName = "CAL_OCL_DISABLE_PROFILING";
+// Set to value >=0 to break in specified CAL process and print PID. This allows to easlily attach a debugger
 constexpr std::string_view calDebugBreakClientOrdinalEnvName = "CAL_DEBUG_BREAK_CLIENT_ORDINAL";
+// By default CAL will abort any API call that uses structures with field pNext not set to nullptr, when the extension is not recognized by CAL. It effectively means, that usage of uninitialized pNext fields leads to abort of an application
 constexpr std::string_view calAbortOnNonNullPNext = "CAL_ABORT_ON_NON_NULL_PNEXT";
