daccf20ec233f4da84243f5014d62dad23086dce
Luzynski, Sebastian Jozef <sebastian.jozef.luzynski@intel.com>
2023-03-14 15:11:12 +0000

    Move IcdL0Device to separate file
    Signed-off-by: Luzynski, Sebastian Jozef <sebastian.jozef.luzynski@intel.com>

--- icd/level_zero/icd_level_zero.cpp
+++ icd/level_zero/icd_level_zero.cpp
@@ -334,63 +334,6 @@ bool IcdL0Platform::isZeAffinityMaskPresent() {
     return zeAffinityMaskPresent;
 }
 
-void IcdL0Device::addSubDeviceToFilter(uint32_t subDeviceIndex) {
-    if (selectedDevices.size() < subDeviceIndex + 1) {
-        selectedDevices.resize(subDeviceIndex + 1);
-    }
-    selectedDevices[subDeviceIndex] = true;
-}
-bool IcdL0Device::isZeAffinityMaskPresent() {
-    std::call_once(parseZeAffinityMaskOnce, [this]() {
-        if (!selectedDevices.empty()) {
-            uint32_t numAllDevices = 0;
-            std::vector<ze_device_handle_t> allDevices;
-            auto status = Cal::Icd::LevelZero::zeDeviceGetSubDevicesRpcHelper(this, &numAllDevices, nullptr);
-            if (status != ZE_RESULT_SUCCESS) {
-                return;
-            }
-
-            allDevices.resize(numAllDevices);
-            status = Cal::Icd::LevelZero::zeDeviceGetSubDevicesRpcHelper(this, &numAllDevices, allDevices.data());
-            if (status != ZE_RESULT_SUCCESS) {
-                return;
-            }
-
-            selectedDevices.resize(numAllDevices);
-
-            for (auto i = 0u; i < numAllDevices; i++) {
-
-                if (selectedDevices[i]) {
-                    filteredDevices.push_back(allDevices[i]);
-                }
-            }
-            zeAffinityMaskPresent = true;
-        }
-    });
-
-    return zeAffinityMaskPresent;
-}
-
-bool IcdL0Device::patchDeviceName(ze_device_properties_t &properties) {
-    const auto nullTerminator = std::find(std::begin(properties.name), std::end(properties.name), '\0');
-    if (nullTerminator == std::begin(properties.name)) {
-        log<Verbosity::critical>("Cannot patch device name! It is empty!");
-        return false;
-    }
-
-    static constexpr char deviceNameSuffix[] = " (aggregated)";
-    static constexpr auto requiredSpace = std::distance(std::begin(deviceNameSuffix), std::end(deviceNameSuffix));
-
-    const auto availableSpace = std::distance(nullTerminator, std::end(properties.name));
-    if (availableSpace < requiredSpace) {
-        log<Verbosity::critical>("Cannot patch device name with '(aggregated)' suffix! Not enough space!");
-        return false;
-    }
-
-    std::copy(std::begin(deviceNameSuffix), std::end(deviceNameSuffix), nullTerminator);
-    return true;
-}
-
 template <typename RemoteL0ObjectT, typename LocalL0ObjectT>
 void objectCleanup(void *remote, void *local) {
     Cal::Icd::icdGlobalState.getL0Platform()->removeObjectFromMap(static_cast<RemoteL0ObjectT>(remote), static_cast<LocalL0ObjectT>(local));
--- icd/level_zero/icd_level_zero.h
+++ icd/level_zero/icd_level_zero.h
@@ -10,6 +10,7 @@
 #include "icd/icd_global_state.h"
 #include "icd/icd_platform.h"
 #include "icd/level_zero/api_customization/icd_level_zero_api.h"
+#include "icd/level_zero/api_type_wrapper/device_wrapper.h"
 #include "icd/level_zero/api_type_wrapper/handles_definitions.h"
 #include "icd/level_zero/api_type_wrapper/kernel_wrapper.h"
 #include "icd/level_zero/api_type_wrapper/module_wrapper.h"
@@ -34,53 +35,6 @@ namespace LevelZero {
 
 template <typename RemoteL0ObjectT, typename LocalL0ObjectT>
 void objectCleanup(void *remote, void *local);
-
-class IcdL0Device : public Cal::Shared::RefCountedWithParent<_ze_device_handle_t, Logic::IcdL0TypePrinter> {
-  public:
-    using RefCountedWithParent::RefCountedWithParent;
-
-    bool patchDeviceName(ze_device_properties_t &properties);
-    void addSubDeviceToFilter(uint32_t subDeviceIndex);
-    bool isZeAffinityMaskPresent();
-    const std::vector<ze_device_handle_t> &getFilteredDevices() const { return filteredDevices; };
-
-    Logic::PropertiesCache::VectorTuple<ze_device_properties_t,
-                                        ze_device_compute_properties_t,
-                                        ze_device_module_properties_t,
-                                        ze_device_memory_access_properties_t,
-                                        ze_device_image_properties_t,
-                                        ze_device_external_memory_properties_t,
-                                        ze_command_queue_group_properties_t,
-                                        ze_device_cache_properties_t,
-                                        ze_device_memory_properties_t>
-        properties;
-
-    struct PropertiesCount {
-        uint32_t commandQueueGroupCount = 0;
-        uint32_t cacheCount = 0;
-        uint32_t memoryCount = 0;
-    } propertiesCount;
-
-    template <typename T>
-    constexpr uint32_t &getPropertiesCount() {
-        if constexpr (std::is_same_v<T, ze_command_queue_group_properties_t>) {
-            return this->propertiesCount.commandQueueGroupCount;
-        }
-        if constexpr (std::is_same_v<T, ze_device_cache_properties_t>) {
-            return this->propertiesCount.cacheCount;
-        }
-        if constexpr (std::is_same_v<T, ze_device_memory_properties_t>) {
-            return this->propertiesCount.memoryCount;
-        }
-        return Logic::PropertiesCache::defaultPropertiesCount;
-    }
-
-  private:
-    bool zeAffinityMaskPresent = false;
-    std::vector<bool> selectedDevices;
-    std::vector<ze_device_handle_t> filteredDevices;
-    std::once_flag parseZeAffinityMaskOnce;
-};
 
 struct IcdL0Context : Cal::Shared::RefCountedWithParent<_ze_context_handle_t, Logic::IcdL0TypePrinter> {
     using RefCountedWithParent::RefCountedWithParent;
