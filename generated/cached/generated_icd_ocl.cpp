47203503ed35b21605ba0c4704247abfa40d0f44
Slawomir Milczarek <slawomir.milczarek@intel.com>
2023-09-13 15:27:05 +0000

    Fixed page fault on non shared memory address
    Fixes page fault in hpc workload a21 hpl
    
    Signed-off-by: Slawomir Milczarek <slawomir.milczarek@intel.com>

--- generated/cached/generated_icd_ocl.cpp
+++ generated/cached/generated_icd_ocl.cpp
@@ -209,7 +209,9 @@ cl_context clCreateContext (const cl_context_properties* properties, cl_uint num
 
     ret = globalPlatform->translateNewRemoteObjectToLocalObject(ret);
     channelLock.unlock();
-    ret->asLocalObject()->setDevicesList(num_devices, devices);
+    if (ret != nullptr) {
+        ret->asLocalObject()->setDevicesList(num_devices, devices);
+    }
     return ret;
 }
 cl_context clCreateContextFromType (const cl_context_properties* properties, cl_device_type device_type, void (CL_CALLBACK* pfn_notify)(const char* errinfo, const void* private_info, size_t cb, void* user_data), void* user_data, cl_int* errcode_ret) {
