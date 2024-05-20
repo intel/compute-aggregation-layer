f7930bc8d70342438d54a890fe3918aaa77ba86e
Milczarek, Slawomir <slawomir.milczarek@intel.com>
2024-05-20 15:14:51 +0000

    feature: Add experimental extension structures for zeDeviceGetModuleProperties
    Add structure definitions: ZEX_STRUCTURE_DEVICE_MODULE_REGISTER_FILE_EXP and
    ZEX_STRUCTURE_DEVICE_MODULE_REGISTER_FILE_EXP, from github sources in public:
    https://github.com/intel/compute-runtime/blob/master
    level_zero/api/driver_experimental/public/zex_module.h
    
    Related-To: NEO-11082
    
    Signed-off-by: Milczarek, Slawomir <slawomir.milczarek@intel.com>

--- third_party/level_zero_headers/driver_experimental/public/zex_module.h
+++ third_party/level_zero_headers/driver_experimental/public/zex_module.h
@@ -22,6 +22,27 @@ zexKernelGetBaseAddress(
     ze_kernel_handle_t hKernel,
     uint64_t *baseAddress);
 
+///////////////////////////////////////////////////////////////////////////////
+/// @brief General Register File descriptor.
+/// Must be passed to zeDeviceGetModuleProperties via pNext member of ze_device_module_properties_t.
+#define ZEX_STRUCTURE_DEVICE_MODULE_REGISTER_FILE_EXP (ze_structure_type_t)0x00030010
+typedef struct _zex_device_module_register_file_exp_t {
+    ze_structure_type_t stype = ZEX_STRUCTURE_DEVICE_MODULE_REGISTER_FILE_EXP; ///< [in] type of this structure
+    const void *pNext = nullptr;                                               ///< [in, out][optional] pointer to extension-specific structure
+    uint32_t registerFileSizesCount;                                           ///< [out] Size of array of supported GRF sizes
+    uint32_t *registerFileSizes = nullptr;                                     ///< [in, out][optional] Array of supported GRF sizes
+} zex_device_module_register_file_exp_t;
+
+///////////////////////////////////////////////////////////////////////////////
+/// @brief Kernel register file size information
+/// Must be passed to zeKernelGetProperties via pNext member of ze_kernel_properties_t
+#define ZEX_STRUCTURE_KERNEL_REGISTER_FILE_SIZE_EXP (ze_structure_type_t)0x00030012
+typedef struct _zex_kernel_register_file_size_exp_t {
+    ze_structure_type_t stype = ZEX_STRUCTURE_KERNEL_REGISTER_FILE_SIZE_EXP; ///< [in] type of this structure
+    const void *pNext = nullptr;                                             ///< [in, out][optional] pointer to extension-specific structure
+    uint32_t registerFileSize;                                               ///< [out] Register file size used in kernel
+} zex_kernel_register_file_size_exp_t;
+
 #if defined(__cplusplus)
 } // extern "C"
 #endif
