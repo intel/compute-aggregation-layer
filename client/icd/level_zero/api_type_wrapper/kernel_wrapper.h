429394da98ac8132807dd5bb8e50f4791de68bfe
Vladimir Polin <Vladimir.Polin@intel.com>
2024-05-29 17:22:25 +0000

    Fixing group size cache for L0
    Resolves: GSD-9213
    Signed-off-by: Vladimir Polin <Vladimir.Polin@intel.com>

--- client/icd/level_zero/api_type_wrapper/kernel_wrapper.h
+++ client/icd/level_zero/api_type_wrapper/kernel_wrapper.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Intel Corporation
+ * Copyright (C) 2023-2024 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -64,7 +64,7 @@ struct IcdL0Kernel : Cal::Shared::RefCountedWithParent<_ze_kernel_handle_t, Logi
         return false;
     }
     void storeSuggestedGroupSizes(uint32_t globalSizeX, uint32_t globalSizeY, uint32_t globalSizeZ, uint32_t groupSizeX, uint32_t groupSizeY, uint32_t groupSizeZ) {
-        suggestedGroupSizes.push_back(SuggestedGroupSizeEntry{GroupSize{globalSizeX, globalSizeY, globalSizeX}, GroupSize{groupSizeX, groupSizeY, groupSizeZ}});
+        suggestedGroupSizes.push_back(SuggestedGroupSizeEntry{GroupSize{globalSizeX, globalSizeY, globalSizeZ}, GroupSize{groupSizeX, groupSizeY, groupSizeZ}});
     }
 };
 
